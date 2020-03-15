874c7724615b90bc80a6763c1e67078fe1668750
rtxrulez <>
2020-03-15 23:21:23 +0300

    feat: morze function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,95 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0"
 };
 
-function decode(expr) {
-    // write your solution here
+String.prototype.splitNative = String.prototype.split;
+
+String.prototype.split = function(separator, limit, chunksize) {
+  if (chunksize === undefined || chunksize === 1)
+    return this.splitNative(separator, limit);
+
+  // кастомное разбиение тут
+  let result = [];
+  for (let i = 0; i < this.length; i += chunksize) {
+    result.push(this.substr(i, chunksize));
+  }
+
+  return result;
+};
+
+function boolean2morze(str) {
+  let arr = str.split("", undefined, 2);
+
+  const morze = arr.map(v => {
+    switch (v) {
+      case "10":
+        return ".";
+      case "11":
+        return "-";
+      case "**":
+        return "*";
+      case "00":
+        return "_";
+      default:
+        return "";
+    }
+  });
+
+  return morze.join("");
 }
 
-module.exports = {
-    decode
+function decode(expr) {
+  let str = "";
+  const morze = boolean2morze(expr);
+  const arr = morze.split(expr, undefined, 5);
+
+  arr.forEach(v => {
+    if (v.indexOf("*") >= 0) {
+      str = str + " ";
+    } else {
+      const key = v.replace(/_/g, "");
+      str = str + MORSE_TABLE[key];
+    }
+  });
+  return str;
 }
+
+// console.log("d", decode(expr));
+module.exports = {
+  decode
+};
